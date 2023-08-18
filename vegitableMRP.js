80f1246eab7cf0cfd86b426bfdc4ecd7dbaf189a
unknown <nikhilppry2001@gmail.com>
2023-08-18 21:03:48 +0530

    first commit

---
+++ vegitableMRP.js
@@ -0,0 +1,13 @@
+
+let potato=(50);
+let tomato=(70);
+let carrot=(80);
+let cabbage=(100);
+let onion=(45);
+let TOTALMRP=(potato+tomato+carrot+cabbage+onion);
+console.log(TOTALMRP);
+if(TOTALMRP<=400){
+    console.log("i accept to buy")
+}else if(TOTALMRP>=400){
+    console.log("i cannot to buy")
+}; 
