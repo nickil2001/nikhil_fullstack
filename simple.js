1c997762c2e2a291c52a04afac74a5fcd690ee5a
nickil2001 <142251816+nickil2001@users.noreply.github.com>
2023-08-19 11:00:12 +0530

    simple js

---
+++ simple.js
@@ -0,0 +1,34 @@
+let a=23;
+ let b=34;
+ console.log(a+b);
+
+//2
+let c=55;
+let d=36;
+result=c-d;
+console.log("result=",result);
+
+//3
+var e=34;
+var f=25;
+console.log("result=",e*f);
+
+//4
+const g=15;
+const h=5;
+result= g/h;
+console.log("result=", result);
+
+//5
+ var x=17;
+ let m=3;
+ y=x%m;
+ console.log("result",y);
+
+ //6
+ const p=45;
+ const q=34;
+ const r=23;
+ const s=3;
+ result=p+q*r-s;
+ console.log("result", result)
